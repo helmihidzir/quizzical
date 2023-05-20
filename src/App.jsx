b53dfc556d869ab53bf67c602b7986f37c851595
helmihidzir <helmihidzir@gmail.com>
2023-05-20 18:29:01 +0800

    Add first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from "react"
 
 export default function App() {
+    const [quiz, setQuiz] = React.useState(false)
+
+    function startQuiz() {
+        setQuiz(true)
+    }
+
     return (
-        <>
-            Hello
-        </>
+        <main>
+            {
+                quiz ?
+                    <>
+                        Start game
+                    </>
+                :
+                    <>
+                        <h1 className="title">Anime Quizzical</h1>
+                        <p className="description">Prove Yourself as a People of Culture</p>
+                        <button className="start-quiz" onClick={startQuiz}>Start quiz</button>
+                    </>
+            }
+        </main>
     )
 }
